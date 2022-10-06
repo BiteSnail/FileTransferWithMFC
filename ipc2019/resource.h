7784227a066295cefb86e3da60f61a79a23482bd
bitesnail <rkwoal216@gmail.com>
2022-10-06 10:39:53 +0900

    Mod winpcap directory path

--- ipc2019/resource.h
+++ ipc2019/resource.h
@@ -21,6 +21,7 @@
 #define IDC_BUTTON_ADDR                 1005
 #define IDC_CHECK1                      1006
 #define IDC_CHECK_TOALL                 1006
+#define IDC_COMBO1                      1007
 
 // Next default values for new objects
 // 
@@ -28,7 +29,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
